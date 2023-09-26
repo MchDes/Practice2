9ed094c871257dcc839df8fe4d9a812b471f99f8
MchDes <stddes76@gmail.com>
2023-09-26 20:36:37 +0300

    Renamed

---
+++ test2.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   const int size = 100;
+   int count;
+   scanf("%d", &count);
+   int start;
+   int arr[size];
+   for (start = 0; start < count; ++start)
+   {
+        scanf("%d ", &arr[start]);
+   }
+   for (int start = 0; start < count; ++start)
+   {
+        printf("%d ", arr[count - start - 1]);
+   }
+}
